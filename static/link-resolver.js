4c94d675b1ebd84233a698b6fd34390adbe858dc
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-03-26 14:18:22 -0500

    Update link-resolver.js

--- static/link-resolver.js
+++ static/link-resolver.js
@@ -20,7 +20,7 @@ void async function LinkResolver(){
     });
     
     async function resolveAll(){
-      
+      cloneStyles();
       transformLinks('href');
       transformLinks('src');
       transformLinks('action');
@@ -54,18 +54,67 @@ void async function LinkResolver(){
         });
     
       }
-    
-      queryApplyAll('['+attr+'^="http://"]:not(link,img)',
-        (el)=>{
-          let char='?';
-          if(el[attr].includes('?')){char='&';}
-             el.updateAttribute(attr,
-                               el[attr].replaceAll("http://","https://"));
-        });
 
+      if(location.protocol=='https://'){
+        queryApplyAll('['+attr+'^="http://"]:not(link,img)',
+          (el)=>{
+            let char='?';
+            if(el[attr].includes('?')){char='&';}
+               el.updateAttribute(attr,
+                                 el[attr].replaceAll("http://","https://"));
+          });
+      }
         
     }
+
+
+  async function cloneStyles(){
+      const hostTargetList_length = globalThis.hostTargetList.length;
+      for(let i=0;i<hostTargetList_length;i++){
+        queryApplyAll('link[href^="https://'+globalThis.hostTargetList[i]+'"]',
+        (el)=>{
+		  let linkClone = el.cloneNode(true);
+          linkClone.setAttribute('clone','clone');
+          el.setAttribute('clone','original');
+          let hash='';
+          if(el['href'].includes('#')){hash='#'+el['href'].split('#')[1];}
+          let char='?';
+          if(el['href'].includes('?')){char='&';}
+             linkClone.setAttribute('href',
+                               el['href'].split('#')[0]
+                                  .replace('https://'+globalThis.hostTargetList[i],
+                                   window.location.origin)+
+                                  char+'hostname='+
+                                  globalThis.hostTargetList[i]+
+                                  '&referer='+window.location.host+
+                                  hash);
+		  el.after(linkClone);	
+        });
     
+      }
+    
+    queryApplyAll('link[href^="/"]:not([clone]),[href^="./"]:not([clone]),link[href^="../"]:not([clone]),link[href]:not([clone],[href*=":"])',
+      (el)=>{
+          let linkClone = el.cloneNode(true);
+          linkClone.setAttribute('clone','clone');
+          el.setAttribute('clone','original');
+          linkClone.setAttribute('href',el.href);
+          el.after(linkClone);
+      });
+      if(location.protocol=='https://'){
+        queryApplyAll('link[href^="http://"]',
+          (el)=>{
+            let linkClone = el.cloneNode(true);
+            linkClone.setAttribute('clone','clone');
+            el.setAttribute('clone','original');
+            let char='?';
+            if(el['href'].includes('?')){char='&';}
+               linkClone.setAttribute('href',
+                                 el['href'].replaceAll("http://","https://"));
+            el.after(linkClone);
+          });
+      }
+  }
     
 }();
   
