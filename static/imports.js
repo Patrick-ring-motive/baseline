03d19e111bb290c7cab9efce28252d04c64db7a6
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-03-26 15:09:53 -0500

    Update imports.js

--- static/imports.js
+++ static/imports.js
@@ -12,6 +12,10 @@ void async function ImportResources(){
     body().appendChild(buildElement('link',{attr:{href:"https://patrick-ring-motive.github.io/baseline/static/colors.css",rel:"stylesheet"}}));
   }
 
+  if(!select('link[href^="https://patrick-ring-motive.github.io/baseline/static/golang.css"]')){
+    body().appendChild(buildElement('link',{attr:{href:"https://patrick-ring-motive.github.io/baseline/static/golang.css",rel:"stylesheet"}}));
+  }
+
   if(!globalThis.LinkResolver){
     import('https://patrick-ring-motive.github.io/baseline/static/link-resolver.js');
   }
