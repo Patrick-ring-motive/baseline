dc46d917a8760c506d239a3cd0fd60c788db2ce4
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-04-15 11:57:22 -0500

    Update cloudflare-custom.js

--- static/cloudflare-custom.js
+++ static/cloudflare-custom.js
@@ -7,4 +7,7 @@ void async function(){
   declare(()=>{   
     queryApplyAll('html[window-location*="?ts"] [href]:not([href*="?ts"])',()=>replaceAttribute('href',/(.*)/,'$1?ts'));
   });
+  declare(()=>{
+    queryApplyAll('html:not([window-location*="?ts"]) .DocsToolbar--tools:not(:has(a[innerText*="Legacy"]))',{prepend:[buildElement('a',{traits:{innerText:'Legacy',href:'https://workers.patrickring.net/workers/?ts'},styles:{'text-decoration':'none'}})]});
+  });
 }();
