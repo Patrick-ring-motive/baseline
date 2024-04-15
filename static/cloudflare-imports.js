38d888f17cbc722a270479708ae9d6825174a4f7
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-04-15 11:22:55 -0500

    Create cloudflare-imports.js

---
+++ static/cloudflare-imports.js
@@ -0,0 +1,20 @@
+
+void async function ImportResources(){
+  if(globalThis.ImportResourcesRunning){return;}
+  globalThis.ImportResourcesRunning=true;
+  if(!globalThis.CloudflareCustomJS){
+    import("https://patrick-ring-motive.github.io/baseline/static/cloudflare-custom.js");
+  }
+  if(!globalThis.declare){
+    await import(`https://patrick-ring-motive.github.io/framework/framework.js?${new Date().getTime()}`);
+  }
+  await DOMInteractive();
+
+  if(!select('link[href^="https://patrick-ring-motive.github.io/baseline/static/cloudflare-custom.css"]')){
+    body().appendChild(buildElement('link',{attr:{href:"https://patrick-ring-motive.github.io/baseline/static/cloudflare-custom.css",rel:"stylesheet"}}));
+  }
+
+  if(!globalThis.LinkResolver){
+    import('https://patrick-ring-motive.github.io/baseline/static/link-resolver.js');
+  }
+}();
