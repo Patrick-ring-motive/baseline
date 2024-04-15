f758c93f7d7992a48fe2243383f6fa64969acdf9
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-04-15 11:57:51 -0500

    Update cloudflare-custom.js

--- static/cloudflare-custom.js
+++ static/cloudflare-custom.js
@@ -8,6 +8,7 @@ void async function(){
     queryApplyAll('html[window-location*="?ts"] [href]:not([href*="?ts"])',()=>replaceAttribute('href',/(.*)/,'$1?ts'));
   });
   declare(()=>{
-    queryApplyAll('html:not([window-location*="?ts"]) .DocsToolbar--tools:not(:has(a[innerText*="Legacy"]))',{prepend:[buildElement('a',{traits:{innerText:'Legacy',href:'https://workers.patrickring.net/workers/?ts'},styles:{'text-decoration':'none'}})]});
+    queryApplyAll('html:not([window-location*="?ts"]) .DocsToolbar--tools:not(:has(a[innerText*="Legacy"]))',
+                  {prepend:[buildElement('a',{traits:{innerText:'Legacy',href:'https://workers.patrickring.net/workers/?ts'},styles:{'text-decoration':'none'}})]});
   });
 }();
