1b23a1ac3753e4641c8ef48278fd210260c91054
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-04-15 12:00:24 -0500

    Update cloudflare-custom.js

--- static/cloudflare-custom.js
+++ static/cloudflare-custom.js
@@ -9,6 +9,6 @@ void async function(){
   });
   declare(()=>{
     queryApplyAll('html:not([window-location*="?ts"]) .DocsToolbar--tools:not(:has(a[innerText*="Legacy"]))',
-                  {prepend:[buildElement('a',{traits:{innerText:'Legacy',href:'https://workers.patrickring.net/workers/?ts'},styles:{'text-decoration':'none'}})]});
+                  {prepend:[buildElement('a',{traits:{innerText:'Legacy',href:'https://workers.patrickring.net/workers/?ts'},styles:{'text-decoration':'none','padding-right':'10%'}})]});
   });
 }();
