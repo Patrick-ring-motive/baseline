e796dc7688b3751efe5a17af5770c21d87357ef0
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-04-15 12:05:10 -0500

    Update cloudflare-custom.js

--- static/cloudflare-custom.js
+++ static/cloudflare-custom.js
@@ -5,10 +5,14 @@ void async function(){
   }
   await DOMInteractive();
   declare(()=>{   
-    queryApplyAll('html[window-location*="?ts"] [href]:not([href*="?ts"])',()=>replaceAttribute('href',/(.*)/,'$1?ts'));
+    queryApplyAll('html[window-location*="?ts"] [href]:not([href*="?ts"],[innerText*="Latest"])',()=>replaceAttribute('href',/(.*)/,'$1?ts'));
   });
   declare(()=>{
     queryApplyAll('html:not([window-location*="?ts"]) .DocsToolbar--tools:not(:has(a[innerText*="Legacy"]))',
                   {prepend:[buildElement('a',{traits:{innerText:'Legacy',href:'https://workers.patrickring.net/workers/?ts'},styles:{'text-decoration':'none','padding-right':'10%'}})]});
+  });
+  declare(()=>{
+    queryApplyAll('html[window-location*="?ts"] .DocsToolbar--tools:not(:has(a[innerText*="Latest"]))',
+                  {prepend:[buildElement('a',{traits:{innerText:'Latest',href:(location.href.replace('?ts',''))},styles:{'text-decoration':'none','padding-right':'10%'}})]});
   });
 }();
