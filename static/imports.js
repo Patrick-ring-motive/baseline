b545a4342ddee3adeaf85df1e906d5a34e62f5f9
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-05-15 12:11:42 -0500

    Update imports.js

--- static/imports.js
+++ static/imports.js
@@ -2,20 +2,14 @@
 void async function ImportResources(){
   if(globalThis.ImportResourcesRunning){return;}
   globalThis.ImportResourcesRunning=true;
-  
-  if(!globalThis.declare){
-    await import(`https://patrick-ring-motive.github.io/framework/framework.js?${new Date().getTime()}`);
-  }
+  globalThis.declare??await import(`https://patrick-ring-motive.github.io/framework/framework.js?${new Date().getTime()}`);
   await DOMInteractive();
-
   if(!select('link[href^="https://patrick-ring-motive.github.io/baseline/static/colors.css"]')){
     body().appendChild(buildElement('link',{attr:{href:"https://patrick-ring-motive.github.io/baseline/static/colors.css",rel:"stylesheet"}}));
   }
-
   if(!select('link[href^="https://patrick-ring-motive.github.io/baseline/static/golang.css"]')){
     body().appendChild(buildElement('link',{attr:{href:"https://patrick-ring-motive.github.io/baseline/static/golang.css",rel:"stylesheet"}}));
   }
-
   if(!globalThis.LinkResolver){
     import('https://patrick-ring-motive.github.io/baseline/static/link-resolver.js');
   }
