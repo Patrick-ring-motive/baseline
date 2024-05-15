a10e32973fba5c7665c50580378b029ac8cde24e
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-05-15 12:09:26 -0500

    Update cloudflare-imports.js

--- static/cloudflare-imports.js
+++ static/cloudflare-imports.js
@@ -2,24 +2,13 @@
 void async function ImportResources(){
   if(globalThis.ImportResourcesRunning){return;}
   globalThis.ImportResourcesRunning=true;
-  if(!globalThis.CloudflareCustomJS){
-    import("https://patrick-ring-motive.github.io/baseline/static/cloudflare-custom.js");
-  }
-  if(!globalThis.declare){
-    await import(`https://patrick-ring-motive.github.io/framework/framework.js?${new Date().getTime()}`);
-  }
+  globalThis.CloudflareCustomJS??import("https://patrick-ring-motive.github.io/baseline/static/cloudflare-custom.js");
+  globalThis.declare??await import(`https://patrick-ring-motive.github.io/framework/framework.js?${new Date().getTime()}`);
   await DOMInteractive();
-
   if(!select('link[href^="https://patrick-ring-motive.github.io/baseline/static/cloudflare-custom.css"]')){
     body().appendChild(buildElement('link',{attr:{href:"https://patrick-ring-motive.github.io/baseline/static/cloudflare-custom.css",rel:"stylesheet"}}));
   }
-  
-
-  
-  if(!globalThis.LinkResolver){
-    import('https://patrick-ring-motive.github.io/baseline/static/link-resolver.js');
-  }
-
+  globalThis.LinkResolver??import('https://patrick-ring-motive.github.io/baseline/static/link-resolver.js');
   await DOMComplete();
   style('div.DocsToolbar--search',{display:'block'});
 }();
