265a6f87b5502783193e493c21d48258667dce99
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-05-15 12:10:16 -0500

    Update cloudflare-custom.js

--- static/cloudflare-custom.js
+++ static/cloudflare-custom.js
@@ -1,8 +1,6 @@
 globalThis.CloudflareCustomJS = true;
 void async function(){
-  if(!globalThis.declare){
-    await import(`https://patrick-ring-motive.github.io/framework/framework.js?${new Date().getTime()}`);
-  }
+  globalThis.declare??await import(`https://patrick-ring-motive.github.io/framework/framework.js?${new Date().getTime()}`);
   await DOMInteractive();
   declare(()=>{   
     queryApplyAll('html[window-location*="?ts"] [href]:not([href*="?ts"],[innerText*="Latest"])',()=>replaceAttribute('href',/(.*)/,'$1?ts'));
